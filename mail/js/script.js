c67172f68ba677ecdfc40ddeeced5a328201eb91
LucaPertosa <luca.pertosa@hotmail.it>
2023-03-13 13:59:39 +0100

    Complete: mail exercise

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,10 +1,16 @@
 // Creare una array contenente le mail autorizate per l'accesso
-
+const allowedEmails = ["email1@example.com", "email2@example.com", "email3@example.com", "email4@example.com", "email5@example.com"];
+console.log(allowedEmails);
 // Chiedere all'utente la sua mail
-
+const userEmail = prompt('Ciao utente! Per continuare inserisci la tua email')
+console.log(userEmail);
 // Controllare se la mail è presente nell'array 
-
-// SE è presente
-//      allora stampo "accesso consentito"
-// Altrimenti
-//      stampo "accesso negato"
+if (allowedEmails.includes(userEmail)) {
+    // SE è presente
+    //      allora stampo "accesso consentito"  
+    console.log('Accesso consentito');
+} else {
+    // Altrimenti
+    //      stampo "accesso negato"
+    console.log('Accesso negato')
+}
