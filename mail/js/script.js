b48f1438c891fc8915389df4875fb3aca285792a
LucaPertosa <luca.pertosa@hotmail.it>
2023-03-13 13:51:02 +0100

    Add: ragionamento logico

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,1 +1,10 @@
-alert('ciao')
+// Creare una array contenente le mail autorizate per l'accesso
+
+// Chiedere all'utente la sua mail
+
+// Controllare se la mail è presente nell'array 
+
+// SE è presente
+//      allora stampo "accesso consentito"
+// Altrimenti
+//      stampo "accesso negato"
